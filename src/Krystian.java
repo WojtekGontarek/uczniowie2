cd73c1fe55a67284cc92d82e87547d285a0b2b5d
Przemek B <pprzemek89@gmail.com>
2024-03-14 13:22:37 +0100

    fgfgfg

---
+++ src/Krystian.java
@@ -0,0 +1,9 @@
+public class Krystian implements Uczen {
+    @Override
+    public void uczSie() {
+        System.out.println("tylko kromka");
+    }
+    public void ogladajKiepskich(){
+        System.out.println("ogladam kiepskich");
+    }
+}
---
+++ src/Roch.java
@@ -0,0 +1,10 @@
+public class Roch implements Uczen {
+
+    @Override
+    public void uczSie(){
+        System.out.println("Blee dkjfkdjkdjk");
+    }
+    public void grajWSzachy(){
+        System.out.println("ddddd");
+    }
+}
